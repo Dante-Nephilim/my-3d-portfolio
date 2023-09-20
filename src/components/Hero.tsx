47bb4a3b39e7abc1b7e09ef7995b49b227af21c3
Dante-Nephilim <sakkara.ps@gmail.com>
2023-09-20 20:34:59 +0530

    update styles on Hero Section

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -4,14 +4,14 @@ export function Hero() {
       <section className="text-black body-font">
         <div className="container mx-auto flex px-5 py-5 md:flex-row flex-col items-center">
           <div className="lg:flex-grow flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
-            <h1 className="title-font tracking-wide sm:text-3xl md:text-6xl xl:text-7xl mb-4 font-bold ">
+            <h1 className="title-font tracking-wide sm:text-3xl md:text-6xl xl:text-7xl mb-4 font-bold">
               Hi, I am Prem Sagar
             </h1>
             <p className="mb-8 leading-relaxed opacity-50">
               Driven by passion and innovation, I craft seamless digital
               experiences using cutting-edge technologies. With expertise
               spanning from frontend to backend, I am well-equipped to transform
-              your visions into reality.{" "}
+              your visions into reality.
             </p>
             <div className="flex justify-center">
               <button className="inline-flex text-white bg-blue-500 py-2 px-4 focus:outline-none hover:bg-opacity-80 rounded text-sm">
@@ -25,51 +25,51 @@ export function Hero() {
           >
             <div className="w-full flex gap-3 justify-center ">
               <img
-                className="w-full h-32 md:h-40 lg:h-48 object-contain rounded-xl"
+                className="w-full h-32 md:h-40 lg:h-48 object-contain rounded-xl shadow-md transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-110"
                 alt="hero"
                 src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/1280px-React-icon.svg.png"
               />
               <img
-                className="w-full h-32 md:h-40 lg:h-48 object-contain rounded-xl"
+                className="w-full h-32 md:h-40 lg:h-48 object-contain rounded-xl shadow-md transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-110"
                 alt="hero"
                 src="https://nodejs.org/static/images/logo-hexagon-card.png"
               />
               <img
-                className="w-full h-32 md:h-40 lg:h-48 object-contain rounded-xl"
+                className="w-full h-32 md:h-40 lg:h-48 object-contain rounded-xl shadow-md transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-110"
                 alt="hero"
                 src="https://upload.wikimedia.org/wikipedia/commons/9/93/MongoDB_Logo.svg"
               />
             </div>
             <div className="w-full flex gap-3 justify-center my-2">
               <img
-                className="w-full h-32 md:h-40 lg:h-48 object-contain rounded-xl"
+                className="w-full h-32 md:h-40 lg:h-48 object-contain rounded-xl shadow-md transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-110"
                 alt="hero"
                 src="https://expressjs.com/images/express-facebook-share.png"
               />
               <img
-                className="w-full h-32 md:h-40 lg:h-48 object-contain rounded-xl"
+                className="w-full h-32 md:h-40 lg:h-48 object-contain rounded-xl shadow-md transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-110"
                 alt="hero"
                 src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/0d/C_Sharp_wordmark.svg/1200px-C_Sharp_wordmark.svg.png"
               />
               <img
-                className="w-full h-32 md:h-40 lg:h-48 object-contain rounded-xl"
+                className="w-full h-32 md:h-40 lg:h-48 object-contain rounded-xl shadow-md transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-110"
                 alt="hero"
                 src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/28/C_Sharp_logo.svg/1200px-C_Sharp_logo.svg.png"
               />
             </div>
             <div className="w-full flex gap-3 justify-center ">
               <img
-                className="w-full h-32 md:h-40 lg:h-48 object-contain rounded-xl"
+                className="w-full h-32 md:h-40 lg:h-48 object-contain rounded-xl shadow-md transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-110"
                 alt="hero"
                 src="https://upload.wikimedia.org/wikipedia/commons/7/7d/Microsoft_.NET_logo.svg"
               />
               <img
-                className="w-full h-32 md:h-40 lg:h-48 object-contain rounded-xl"
+                className="w-full h-32 md:h-40 lg:h-48 object-contain rounded-xl shadow-md transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-110"
                 alt="hero"
                 src="https://angular.io/assets/images/logos/angular/angular.png"
               />
               <img
-                className="w-full h-32 md:h-40 lg:h-48 object-contain rounded-xl"
+                className="w-full h-32 md:h-40 lg:h-48 object-contain rounded-xl shadow-md transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-110"
                 alt="hero"
                 src="https://upload.wikimedia.org/wikipedia/commons/8/87/Sql_data_base_with_logo.png"
               />
