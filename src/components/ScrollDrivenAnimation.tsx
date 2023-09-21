815511053dcd2c8412374cf9e9a7f8f5ddbcbd9b
Dante-Nephilim <sakkara.ps@gmail.com>
2023-09-22 03:49:23 +0530

    add moon

--- src/components/ScrollDrivenAnimation.tsx
+++ src/components/ScrollDrivenAnimation.tsx
@@ -4,6 +4,8 @@ import ScrollMagic from "scrollmagic";
 import diffuseImagePath from "../assets/8081_earthmap2k.jpg";
 import normalImagePath from "../assets/8081_earthlights2k.jpg";
 import roughnessImagePath from "../assets/brown_mud_leaves_01_rough_4k.jpg";
+import moonDiffusePath from "../assets/moonmap4k.jpg"; // Replace with your image path
+// Add more textures for the moon if needed
 
 export function ScrollDrivenAnimation() {
   const mountRef = useRef<HTMLDivElement>(null);
@@ -13,17 +15,12 @@ export function ScrollDrivenAnimation() {
     const domElement = mountRef.current;
 
     const scene = new THREE.Scene();
-    const camera = new THREE.PerspectiveCamera(
-      75,
-      1920 / 1080, // Desired aspect ratio
-      0.1,
-      1000
-    );
-    camera.updateProjectionMatrix(); // Update after changing aspect
+    const camera = new THREE.PerspectiveCamera(75, 1920 / 1080, 0.1, 1000);
+    camera.updateProjectionMatrix();
 
     const renderer = new THREE.WebGLRenderer({ antialias: true, alpha: true });
-    renderer.setPixelRatio(window.devicePixelRatio); // Increase render clarity
-    renderer.setSize(1920, 1080); // Set to desired resolution
+    renderer.setPixelRatio(window.devicePixelRatio);
+    renderer.setSize(1920, 1080);
     renderer.shadowMap.enabled = true;
     renderer.shadowMap.type = THREE.PCFSoftShadowMap;
     domElement.appendChild(renderer.domElement);
@@ -38,33 +35,45 @@ export function ScrollDrivenAnimation() {
       roughnessMap: roughnessTexture,
     });
 
-    const geometry = new THREE.SphereGeometry(2, 128, 128); // Increased geometry resolution
+    const geometry = new THREE.SphereGeometry(3, 128, 128);
 
     const sphere = new THREE.Mesh(geometry, material);
-    sphere.castShadow = true;
-    sphere.receiveShadow = true;
-    sphere.position.x = 0; // Centered
     scene.add(sphere);
 
-    camera.position.z = 5;
+    const moonDiffuse = new THREE.TextureLoader().load(moonDiffusePath);
+    const moonMaterial = new THREE.MeshStandardMaterial({
+      map: moonDiffuse,
+      // Add more material properties for the moon if needed
+    });
 
-    // Lights
+    const moonGeometry = new THREE.SphereGeometry(0.5, 64, 64); // Smaller sphere for the moon
+    const moon = new THREE.Mesh(moonGeometry, moonMaterial);
+    moon.position.set(4, 0, 0); // Set initial position of the moon relative to Earth
+    scene.add(moon);
+
+    camera.position.z = 10; // Adjusted to fit both Earth and moon in view
+
     const ambientLight = new THREE.AmbientLight(0xffffff, 0.4);
     scene.add(ambientLight);
 
     const directionalLight = new THREE.DirectionalLight(0xffffff, 0.8);
     directionalLight.position.set(0, 2, 2);
-    directionalLight.castShadow = true;
     scene.add(directionalLight);
 
     const pointLight = new THREE.PointLight(0xff0000, 0.6, 100);
     pointLight.position.set(-2, 2, 2);
-    pointLight.castShadow = true;
     scene.add(pointLight);
+
+    let moonAngle = 0; // Initialize angle for moon's orbit
 
     const animate = () => {
       requestAnimationFrame(animate);
-      sphere.rotation.y += 0.01; // Rotation around Y-axis only
+      sphere.rotation.y += 0.01;
+
+      moonAngle += 0.02; // Adjust this for faster/slower moon rotation
+      moon.position.x = 4 * Math.cos(moonAngle);
+      moon.position.z = 4 * Math.sin(moonAngle);
+
       renderer.render(scene, camera);
     };
     animate();
@@ -74,6 +83,7 @@ export function ScrollDrivenAnimation() {
     const updateSphereRotation = (progress: number) => {
       const angle = progress * Math.PI * 2;
       sphere.rotation.y = angle;
+      // Optionally, adjust moon rotation on scroll as well
     };
 
     new ScrollMagic.Scene({
